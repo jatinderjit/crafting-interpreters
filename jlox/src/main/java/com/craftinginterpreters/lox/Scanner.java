fccca34aa92c708bb74149933c13fe17473d6e7d
Jatinderjit Singh <jatinderjit89@gmail.com>
2023-07-01 22:19:14 +0530

    Implement Scanner

--- jlox/src/main/java/com/craftinginterpreters/lox/Scanner.java
+++ jlox/src/main/java/com/craftinginterpreters/lox/Scanner.java
@@ -1,7 +1,9 @@
 package com.craftinginterpreters.lox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static com.craftinginterpreters.lox.TokenType.*;
 
@@ -11,6 +13,25 @@ public class Scanner {
     private int start = 0;
     private int current = 0;
     private int line = 1;
+
+    private static final Map<String, TokenType> keywords = new HashMap<>() {{
+        put("and", AND);
+        put("class", CLASS);
+        put("else", ELSE);
+        put("false", FALSE);
+        put("fun", FUN);
+        put("for", FOR);
+        put("if", IF);
+        put("nil", NIL);
+        put("or", OR);
+        put("print", PRINT);
+        put("return", RETURN);
+        put("super", SUPER);
+        put("this", THIS);
+        put("true", TRUE);
+        put("var", VAR);
+        put("while", WHILE);
+    }};
 
     Scanner(String source) {
         this.source = source;
@@ -53,13 +74,49 @@ public class Scanner {
             case ' ', '\r', '\t' -> {
             }
             case '\n' -> line++;
-            default -> Lox.error(line, "Unexpected character.");
+            case '"' -> string();
+            default -> {
+                if (isDigit(c)) {
+                    number();
+                } else if (isAlpha(c)) {
+                    identifier();
+                } else {
+                    Lox.error(line, "Unexpected character.");
+                }
+            }
         }
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') ||
+                (c >= 'A' && c <= 'Z') ||
+                c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+
+    private boolean isAtEnd() {
+        return current >= source.length();
     }
 
     private char peek() {
         if (isAtEnd()) return '\0';
         return source.charAt(current);
+    }
+
+    private char advance() {
+        return source.charAt(current++);
+    }
+
+    private char peekNext() {
+        if (current + 1 >= source.length()) return '\0';
+        return source.charAt(current + 1);
     }
 
     private boolean match(char expected) {
@@ -78,11 +135,46 @@ public class Scanner {
         tokens.add(new Token(type, text, literal, line));
     }
 
-    private char advance() {
-        return source.charAt(current++);
+    /**
+     * Parses the double-quoted strings and adds the token along with the literal.
+     * <p>
+     * Escape sequences (such as \n, \t, etc.) are not supported. Currently, there
+     * is no way to add the double quote ('"') character in the string.
+     * <p>
+     * Multi-line strings are supported.
+     */
+    private void string() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') line++;
+            advance();
+        }
+        if (isAtEnd()) {
+            Lox.error(line, "Unterminated string.");
+            return;
+        }
+
+        advance(); // Closing '"'
+
+        // Strip the surrounding quotes.
+        String value = source.substring(start + 1, current - 1);
+        addToken(STRING, value);
     }
 
-    private boolean isAtEnd() {
-        return current >= source.length();
+    private void number() {
+        while (isDigit(peek())) advance();
+
+        if (peek() == '.' && isDigit(peekNext())) {
+            advance(); // consume the dot
+            while (isDigit(peek())) advance();
+        }
+        Double value = Double.parseDouble(source.substring(start, current));
+        addToken(NUMBER, value);
+    }
+
+    private void identifier() {
+        while (isAlphaNumeric(peek())) advance();
+        String text = source.substring(start, current);
+        TokenType type = keywords.getOrDefault(text, IDENTIFIER);
+        addToken(type);
     }
 }
