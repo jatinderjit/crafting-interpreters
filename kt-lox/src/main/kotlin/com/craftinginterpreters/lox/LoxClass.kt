1e8c43321d68e23eec1a2ce7a4daa57b78c340e3
Jatinderjit Singh <jatinderjit89@gmail.com>
2024-03-01 23:56:34 +0530

    kt: Fix method inheritance

--- kt-lox/src/main/kotlin/com/craftinginterpreters/lox/LoxClass.kt
+++ kt-lox/src/main/kotlin/com/craftinginterpreters/lox/LoxClass.kt
@@ -18,7 +18,7 @@ class LoxClass(
     }
 
     fun findMethod(name: String): LoxFunction? =
-        methods[name] ?: superclass?.methods?.get(name)
+        methods[name] ?: superclass?.findMethod(name)
 
     override fun toString(): String =
         "<class: $name>"
