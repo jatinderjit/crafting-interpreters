8b24e125a70f9d8e66c664388110a65c9582aaf2
Jatinderjit Singh <jatinderjit89@gmail.com>
2024-03-04 22:03:18 +0530

    c: Fix grow lines and reallocate size calculation

--- clox/chunk.c
+++ clox/chunk.c
@@ -18,7 +18,7 @@ void writeChunk(Chunk *chunk, uint8_t byte, int line) {
     chunk->capacity = GROW_CAPACITY(oldCapacity);
     chunk->code =
         GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
-    chunk->lines = GROW_ARRAY(int, chunk->code, oldCapacity, chunk->capacity);
+    chunk->lines = GROW_ARRAY(int, chunk->lines, oldCapacity, chunk->capacity);
   }
   chunk->code[chunk->count] = byte;
   chunk->lines[chunk->count] = line;
